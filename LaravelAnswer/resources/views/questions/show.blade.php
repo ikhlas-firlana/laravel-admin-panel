4d305f347417c84c3266a375b306f0b3f6f536d4
ikhlas firlana <ikhlas.firlana@gmail.com>
2018-06-28 16:10:49 +0700

    to be continue

--- LaravelAnswer/resources/views/questions/show.blade.php
+++ LaravelAnswer/resources/views/questions/show.blade.php
@@ -12,17 +12,19 @@
 
         </div>
     </div>
+    @if($question->answers->count() > 0)
     <div class="container">
-    @foreach($question->answers as $answer)
-    <div class="panel panel-default">
-        <div class="panel-body">
-            <p>
-                {{$answer['content']}}
-            </p>
+        @foreach($question->answers as $answer)
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <p>
+                    {{$answer['content']}}
+                </p>
+            </div>
         </div>
+        @endforeach
     </div>
-    @endforeach
-    </div>
+    @endif
     <div class="container">
         <h3>Submit your answer :</h3>
         <form method="post" action="{{ route('answers.store') }}">
--- LaravelAnswer/routes/web.php
+++ LaravelAnswer/routes/web.php
@@ -28,3 +28,7 @@ Route::get('/voyager', function () {
 Route::group(['prefix' => 'voyager-admin'], function () {
     Voyager::routes();
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
